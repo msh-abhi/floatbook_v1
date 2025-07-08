0aad3eb7a8657f3316bf4492a6f8792e2a567463
Shohrab hossain <38214110+msh-abhi@users.noreply.github.com>
2025-07-08 11:54:29 +0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+// src/main.tsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { AuthProvider } from './hooks/useAuth.tsx'; // Import AuthProvider
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider> {/* Wrap your App component */}
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
