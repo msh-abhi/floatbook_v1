0f5fa191188ebd3b17adf4e8afb2f7cff8fc3208
Shohrab hossain <38214110+msh-abhi@users.noreply.github.com>
2025-07-08 16:34:31 +0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 // src/main.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { AuthProvider } from './hooks/useAuth.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
